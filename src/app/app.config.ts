e3b5c316243afbc5171a97598316ea34135ac784
Vladimir Erokhin <erohinvova01010111@gmail.com>
2025-07-23 18:00:24 +0300

    Configured project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,42 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig, LOCALE_ID,
+  provideAppInitializer,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideEventPlugins } from '@taiga-ui/event-plugins';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData} from '@angular/common';
+import localeRu from '@angular/common/locales/ru';
+import {TUI_LANGUAGE, TUI_RUSSIAN_LANGUAGE} from '@taiga-ui/i18n';
+import {of} from 'rxjs';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideAnimations(),
+    provideEventPlugins(),
+    {
+      provide: TUI_LANGUAGE,
+      useValue: of(TUI_RUSSIAN_LANGUAGE),
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'ru-RU'
+    },
+    {
+      provide: DATE_PIPE_DEFAULT_OPTIONS,
+      useValue: {
+        dateFormat: 'dd.mm.yyyy',
+        timezone: '+0300',
+      }
+    },
+    provideAppInitializer(() => {
+      registerLocaleData(localeRu);
+    })
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
+import {TuiRoot} from "@taiga-ui/core";
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TuiRoot],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('bookIT-admin-front');
-}
+export class App {}
